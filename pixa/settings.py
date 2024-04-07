e63a023bfc983af3e57e1a4964ba4710801a7fbd
sourav-singh-rawat <souravrawat1999@gmail.com>
2024-04-07 17:29:36 +0530

    Change debug status

--- pixa/settings.py
+++ pixa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nrn$x*db@h5-3k985_ypi5jci6&kboex6h@53$0phktx&%-+ir'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['34.93.105.92']
 
